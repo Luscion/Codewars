b0ec9ebdb9b712afb8b6e7dea4abb716fa3c9e2c
unknown <illuscionz@gmail.com>
2023-10-30 08:32:47 -0400

    New codewars challenge

---
+++ 8kyu/FIXME Replace all dots.js
@@ -0,0 +1,23 @@
+/*
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+Task
+
+Fix the bug so we can all go home early.
+Notes
+
+String str will never be null.
+Debugging
+*/
+var replaceDots = function(str) {
+  return str.replace(/./, '-');
+}
+
+
+
+// My solution
+
+let replaceDots = function(str) {
+    return str.split('.').join('-')
+  }
