3eb27361dc1dc0d69d570858cdc63b794f6220a9
unknown <illuscionz@gmail.com>
2023-11-06 18:53:03 -0400

    added new codewars

---
+++ 8kyu/Return to Sanity.js
@@ -0,0 +1,15 @@
+//This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+    let results =
+      {sanity: 'Hello'};
+    return
+      results;
+  }
+
+
+//My Solution
+function mystery() {
+    let results = {sanity: 'Hello'};
+    return results;
+  }
