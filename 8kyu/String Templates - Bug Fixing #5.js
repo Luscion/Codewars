9107abf56b7ea7dc3011b2f4de3eebed49af9d51
unknown <illuscionz@gmail.com>
2023-11-14 11:56:33 -0400

    Added debugging 8kyu coding challenges

---
+++ 8kyu/String Templates - Bug Fixing #5.js
@@ -0,0 +1,15 @@
+//Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+
+//Problem
+function buildString(...template){
+    return `I like #{template.join(',')}!`;
+  }
+
+
+
+
+//My solution
+function buildString(...template){
+    return `I like ${template.join(', ')}!`;
+  }
